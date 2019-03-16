f3e9f4a56114888e9e975cbd87b3c5243e51c319
Fortune Ekeruo <legalmody@gmail.com>
2019-03-16 22:44:10 +0100

    Mock a switch statement and a loop determine what component to display the different data types'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,36 @@ export class AppComponent {
   title = 'json-tree-like-viewer';
   source = source;
 
-  constructor() {}
+  constructor() {
+    this.displayData(this.source);
+  }
+
+  displayData(data) {
+    for (const key in data) {
+      if (typeof data[key] === 'object' && data[key] !== null) {
+        // Display current obj as a bullet before calling displayData recursively
+        this.returnComponent(key, data[key]);
+        this.displayData(data[key]);
+      } else {
+        // Display data in the appropriate component
+        this.returnComponent(key, data[key]);
+      }
+    }
+  }
+
+  returnComponent(key, value) {
+    switch (typeof value) {
+      case 'string': {
+        return 'TextInput Component';
+      }
+
+      case 'number': {
+        return 'NumberInput Component';
+      }
+
+      case 'boolean': {
+        return 'Checkbox Component';
+      }
+    }
+  }
 }
