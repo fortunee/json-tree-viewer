204aa0f9bd81894d7047024fee1f44f4e1f9c1ff
Fortune Ekeruo <legalmody@gmail.com>
2019-03-17 23:56:33 +0100

    Generate number input and checkbox components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { DomService } from './services/dom.service';
+import { NumberInputComponent } from './components/number-input/number-input.component';
+import { CheckBoxComponent } from './components/check-box/check-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextInputComponent
+    TextInputComponent,
+    NumberInputComponent,
+    CheckBoxComponent
   ],
   imports: [
     BrowserModule
